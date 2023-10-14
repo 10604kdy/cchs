99c738c4dd602e25f13cc14e679af9b0a0d2ff26
10604kdy <n.01062234594@gmail.com>
2023-10-14 11:41:21 +0900

    백준준비

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello world")
+a= list(range(0,11))
+print(a[1])
+a[1]=100
+print(a[1])
+
+
+x=[1,2,3,4,5]
+for n in a:
+    print(n)
+
+for nbr in range(1,6):
+ print(nbr)
+
+ i=0
+ while i <5:
+    print(x[i])
+    i+=1
